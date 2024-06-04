9025df81a887619bb88a75d1f9f2cf0522ff4ea9
Chany4 <chandreleighdavids04052005@gmail.com>
2024-06-04 15:39:59 +0200

    fixed

--- script/script.js
+++ script/script.js
@@ -1,21 +1,6 @@
-// // Add Up Task
-// const numberss = [1,2,3,4,5]
-// const sum = numberss.reduce((total,num)=> total + num,0);
-
-// let btn1 = document.querySelector('.inc');
-// let span = document.querySelector('.addPara')
-
-// btn1.addEventListener('click', ()=>{
-//     span.innerHTML = sum
-// })
-
 document.querySelector('.inc').addEventListener('click', function() {
-    // Get the input value
     const input = document.getElementById('numbersInput').value;
-    // Split the input string into an array of numbers
     const numbers = input.split(',').map(Number);
-    // Calculate the sum of the numbers
     const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-    // Display the result in the paragraph tag
     document.querySelector('.addPara').innerText = sum;
 });
